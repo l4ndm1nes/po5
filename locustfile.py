b815bbd06bb33cdf6396fb769c6572fa4f4c09e1
l4ndm1nes <havrylenko.roman@lll.kpi.ua>
2026-04-12 17:35:37 +0300

    1

--- locustfile.py
+++ locustfile.py
@@ -1,6 +1,7 @@
 from locust import HttpUser, task, between
 class WebUser(HttpUser):
-    wait_time = between(0.05, 0.2)
+    wait_time = between(0.05,
+                        0.2)
     @task(5)
     def index(self): self.client.get("/", headers={"Connection":"close"})
     @task(2)
